4f199329f5674f279e073a605a17210fa1372ac3
IsmarySalgesM <ismarysalges@gmail.com>
2018-09-12 20:11:05 -0300

    select funcionando

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,89 +3,35 @@ let second = () => {
   document.getElementById("firstSection").style.display = "none";
   document.getElementById("secondSection").style.display = "block";
 };
+let prueba = [];
 
 window.onload = () => {
   setTimeout(second, 2000);
+
+  let select = document.getElementById("containerInfo");
+  //aqui es la funcion de mi select
+  select.addEventListener('change', function () {
+
+    let selectedOption = select.options[select.selectedIndex].value;
+    console.log(JSON.stringify(selectedOption));
+  });
+
+    let placeFound = prueba.find(element =>{
+      console.log(element)
+      return element.name == site.name
+    });
+
+
 };
 
-//Aqui se imprime información de busqueda
+
 function placeD(site) {
+  let datos = prueba.push(site);
 
-  //console.log(site);
-  // Aqui guardo toda mis opciones de restaurantes
-  let prueba = []
-  let datos = prueba.push(site)
-
-  //aqui recorro todas mis opciones
   let select = document.getElementById("containerInfo");
-  for (let i = 0; i < prueba.length; i++) {
-    select.options[select.options.length] = new Option(prueba[i].name)
-    // console.log(prueba[i].name)
-    //console.log(pruebaSearch);
-  }
-  //aqui es la funcion de mi select
-  select.addEventListener('change',function (event) {
-      console.log(event)
-      let selectedOption = this.options[select.selectedIndex]
-      
-      for (let key in site) {
-        // solo tomo el valor de la direcciones
-        let directionAddress = site.formatted_address
-        //console.log(directionAddress);
-      }
-    }
-  )
+  select.options[select.options.length] = new Option(site.name, site.formatted_address);
 }
-
-           // let tony = site.formatted_address;
-            //console.log(tony);
-         // }
-
-          //console.log(key);
-         // if (site.hasOwnProperty(key)) {
-          //  const element = site.formatted_address;
-
-
-    //console.log(selectedOption.text);
-
-
-
-
-  //let pruebaArray = [];
-  //let prueba = Object.entries(site);
-  //let puching = pruebaArray.push(prueba);
-  //console.log(pruebaArray);
-  //pruebaArray.forEach(element => {
-  //element.forEach(datos => {
-  //et date = datos[1]
-  //console.log(date);
-   // })
-   //})
-
- // }
-
-
- // var select = document.getElementById("containerInfo");
- // for (var i = 0; i < pruebaArray.length; i++) {
-   //select.options[select.options.length] = //console.log(new Option(pruebaArray[i]))
-
-
-
-
-  //for (let i = 0; i < pruebaArray.length; i++) {
-  // const element = pruebaArray[i];
-  //console.log(element);
-//  containerInfo.innerHTML = pruebaArray;
-
-
-
-  //pruebaArray.forEach(element => {
-   //
-   //console.log(element)
- // });
-  //console.log(pruebaArray);
-
-
+     
   // 
   //<div class="card">
   //<div class="card-header" href="${site.place_id}">
